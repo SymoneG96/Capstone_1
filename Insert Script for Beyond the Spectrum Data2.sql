2fb534abfb9b03eed659ebd9e50134355f595b69
Student <sgant1@appdev.yearup.org>
2024-10-17 15:15:58 -0500

    Updates to project, market data research file next

---
+++ Insert Script for Beyond the Spectrum Data2.sql
@@ -0,0 +1,7 @@
+SELECT * FROM beyondthespectrum.services;
+
+INSERT INTO SERVICES (ServiceID, Name, Description, ProviderID, Duration, Category)
+VALUES
+  (1, 'ABA Therapy', 'Applied Behavior Analysis for individuals with autism', 4587, 365, 'ABA Therapy'),
+  (2, 'Speech Therapy', 'Therapy to improve communication and language skills', 1214, 150, 'Speech Therapy'),
+  (3, 'Occupational Therapy', 'Therapy to improve daily living skills and fine motor skills', 2456, 90, 'Occupational Therapy'); 
